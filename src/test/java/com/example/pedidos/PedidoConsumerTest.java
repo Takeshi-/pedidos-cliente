9720d9f129b9cd4d8053d8600611aac9e5a3e6bd
Takeshi- <horitakeshi14@gmail.com>
2025-08-13 21:05:00 -0300

    fix: correct json for test

--- src/test/java/com/example/pedidos/PedidoConsumerTest.java
+++ src/test/java/com/example/pedidos/PedidoConsumerTest.java
@@ -15,15 +15,14 @@ class PedidoConsumerTest {
 
     @Test
     void deveCalcularValorTotalEChamarSave() throws Exception {
-        String json = """        {
-          "codigoPedido": 1001,
-          "codigoCliente": 1,
-          "itens": [
-            {"produto":"lápis","quantidade":100,"preco":1.10},
-            {"produto":"caderno","quantidade":10,"preco":1.00}
-          ]
-        }
-        """;
+        String json = "{"+
+          "\"codigoPedido\": 1001,"+
+          "\"codigoCliente\": 1,"+
+          "\"itens\": ["+
+            "{\"produto\":\"lápis\",\"quantidade\":100,\"preco\":1.10},"+
+            "{\"produto\":\"caderno\",\"quantidade\":10,\"preco\":1.00}"+
+          "]"+
+        "}";
 
         PedidoService service = Mockito.mock(PedidoService.class);
         ObjectMapper mapper = new ObjectMapper();
